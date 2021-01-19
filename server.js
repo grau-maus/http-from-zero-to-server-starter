19d5f57d4fa7d2b5943216108a0d957325a0b645
josh <joshtupas@gmail.com>
2021-01-19 15:22:20 -0600

    added url stuff

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const http = require("http");
 
 http.createServer((request, response) => {
+    console.log("test");
+
+    if (request.url === "/OK") {
+        console.log("Request accepted.");
+        response.writeHead(200);
+
+
+    } else {
+        response.writeHead(404);
+        console.log("Resource not found.");
+    }
+
     response.end();
 }).listen(3000, () => {
     console.log(`listening on port ${3000}`);
